8badd20c48e59b20c33c5f2b2885324d8f948da6
Adenekan Emmanuel <emmanueladenekan20@gmail.com>
2023-09-11 15:32:03 -0700

    lest

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,12 +9,12 @@
  */
 int main(void)
 {
-	char letter;
+    char letter;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
-	putchar('\n');
+    for (letter = 'z'; letter >= 'a'; letter--)
+        putchar(letter);
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
 
